57b460a72130ef2e4e6c1e46b61b72970ca300a1
Osakhra <Evyosakhra@gmail.com>
2026-05-20 21:09:38 -0500

    quiet update

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -326,7 +326,7 @@ export default function AboutPage() {
               },
               {
                 title: 'Competitive Gaming',
-                body: "I play games the same way I enjoy work, with the goal to understand the task at hand completely and master it fully. I play with highly competitive friends who together, bring out the best in each of us as we climb to the ranked ladder. ",
+                body: "I play games the same way I enjoy work, with the goal to understand the task at hand completely and master it fully. I play with highly competitive friends who together, bring out the best in each of us as we climb the ranked ladders. ",
               },
               {
                 title: 'Travel as Default',
